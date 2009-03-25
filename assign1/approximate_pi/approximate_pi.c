be4b176afb3813e5711a52089b12dfb5e6a11edf
Neer Friedman <neer@neer-laptop.(none)>
2009-03-25 21:29:58 +0200

    added basic structure for assign1

---
+++ assign1/approximate_pi/approximate_pi.c
@@ -0,0 +1,16 @@
+/*
+//============================================================================
+// Name        : soft-proj09b.cpp
+// Author      : Neer Friedman
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C, Ansi-style
+//============================================================================
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv) {
+	puts("!!!Hello World!!!");
+	return EXIT_SUCCESS;
+}
