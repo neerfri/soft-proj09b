b9d2cc6449aba105fb0ec3771cab1019ab089d5f
Neer Friedman <neer@neer-laptop.(none)>
2009-03-30 18:04:38 +0300

    finished, wrong results

--- assign1/approximate_pi/approximate_pi.c
+++ assign1/approximate_pi/approximate_pi.c
@@ -10,20 +10,49 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+struct MyPoint {
+	double x;
+	double y;
+};
+
+double approximate_pi(int number_of_runs);
+extern double pow(double, double);
 
 int number_of_points_in_circle(int num_of_samples) {
 	double x, y;
 	x = (double)rand()/RAND_MAX;
 	y = (double)rand()/RAND_MAX;
 	printf("%f, %f\n",x ,y);
-	
-	if (1) {
-		
+	return(0);
+}
+
+void randomize_point(struct MyPoint *p) {
+	(*p).x = (double)rand()/RAND_MAX;
+	(*p).y = (double)rand()/RAND_MAX;
+	/*printf("[%f, %f]\n",(*p).x ,(*p).y);*/	
+}
+
+double approximate_pi(int number_of_runs) {
+	struct MyPoint p;
+	int i, count = 0;
+	double pi;
+	for (i=0; i<number_of_runs; i++) {
+		randomize_point(&p);
+		if ((pow(p.x, 2.0) + pow(p.y, 2.0)) <= 1) {
+			count++;
+		}
 	}
-	
+	pi = (double)count/number_of_runs;
+	pi = pi * 4;
+	return(pi);
 }
 
 int main(void) {
+	int i, n;
 	srand(1234);
+	for(i=0; i<8; i++) {
+		n = pow(10,i+1);
+		printf("%f\n", approximate_pi(n));
+	}
 	return(0);
 }
