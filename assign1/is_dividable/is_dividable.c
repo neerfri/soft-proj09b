a2f78f5cd730d4a2efe02fa2a7f1694b9ff3d7ba
Neer Friedman <neer@neer-laptop.(none)>
2009-03-31 22:46:34 +0300

    finished is_dividable

--- assign1/is_dividable/is_dividable.c
+++ assign1/is_dividable/is_dividable.c
@@ -4,13 +4,32 @@
 // Author      : Neer Friedman
 // Version     :
 // Copyright   : Your copyright notice
-// Description : Hello World in C, Ansi-style
+// Description : Checks division by 3 of a number
 //============================================================================
 */
 #include <stdio.h>
 #include <stdlib.h>
 
+int digits_sum(int n) {
+	int sum = 0;
+	while(n > 0) {
+		sum = sum + (n % 10);
+		n = n/10;
+	}
+	return(sum);
+}
+
 int main(void) {
-	puts("!!!Hello World!!!");
+	int n;
+	scanf("%d", &n);
+	while(n > 9) {
+		printf("%d\n", n);
+		n = digits_sum(n);
+	}
+	printf("%d\n", n);
+	if (n == 9 || n==6 || n==3)
+		printf("DIVIDABLE");
+	else
+		printf("NOT DIVIDABLE");
 	return EXIT_SUCCESS;
 }
