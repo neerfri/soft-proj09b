ad4f8ed2b46fe25d08ba75610300d60c53b3628f
Lea Stolowicz <leastolo@localhost.localdomain>
2009-04-01 00:20:32 +0300

    Finished check_policy.c

--- assign1/check_policy/check_policy.c
+++ assign1/check_policy/check_policy.c
@@ -14,12 +14,9 @@ int main(void) {
 	
 	int ch;
 	int is_length = 0; /* will be true if length od password is between 8 and 14 */
-	int is_upper = 0; /* will be true if the password starts with an upper letter 
-							65-90 in ascii code */
-	int is_digit = 0;  /* true if contains at least one digit 
-					 48-57 in ascii code */
-	int is_lower_case = 0; /* true if contains at least one lower case letter
-							 97-122 in ascii code */
+	int is_upper = 0; /* will be true if the password starts with an upper letter */
+	int is_digit = 0;  /* true if contains at least one digit */
+	int is_lower_case = 0; /* true if contains at least one lower case letter */
 	int is_character = 0; /* true if contains at least one char which is not a digit or a letter */
 	int length = 0;
 	
@@ -27,45 +24,40 @@ int main(void) {
 	if (('A' <= ch) && (ch <= 'Z'))
 	{
 		is_upper = 1;
-	} else 
+	} 
+	else 
 	{
-		printf("BAD PASSWORD\n");
-		printf("First letter in not upper case\n");
+		printf("BAD PASSWORD");
 		return 0;
 	}
 		
 	do
 	{
 		ch = getchar();
-		
-		if ('0' <= ch) && (ch <= '9'))
-			is_digit = 1;
-		if (('a' <= ch) && (ch <= 122))
-			is_lower_case = 1;
-		if (!('0' <= ch) && (ch <= '9')) && (!97 <= ch <= 122))
-			is_character = 1;
+		if (ch != '\n')
+		{
+			if (('0' <= ch) && (ch <= '9'))
+				is_digit = 1;
+			if (('a' <= ch) && (ch <= 'z'))
+				is_lower_case = 1;
+			if ((!(('0' <= ch) && (ch <= '9'))) 
+				&& (!(('a' <= ch ) && (ch  <= 'z')))
+				&&(!(('A' <= ch) && (ch <= 'Z'))))
+				is_character = 1;
+		}
 		++length;
-	
-		printf ("** %d **", ch);
 	}
 	while (ch != '\n');
-	
-	printf ("length:%d\n", length);
+	if ((length >= 8) && (length <=14))
+		is_length= 1;
 	
 	if (is_length && is_upper && is_digit && is_lower_case && is_character)
 	{
-		printf("GOOD PASSWORD\n");
+		printf("GOOD PASSWORD");
 	}
 	else
 	{
-		printf("BAD PASSWORD\n");
-		if (is_length == 0)
-			printf("Password len not between 8 and 14\n");
-		if (is_lower_case == 0)
-			printf("No lower case letters in password\n");
-		if (is_character == 0)
-		 printf("Password len not between 8 and 14\n");
-	}
-	
+		printf("BAD PASSWORD");
+	}	
 	return 0;
 }
