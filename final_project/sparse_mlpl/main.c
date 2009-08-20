d773ada6debfd3c4b5ebca7215490f2748de311b
neer <neer@neer-laptop.(none)>
2009-08-20 18:37:31 +0300

    fixing sparse_mlpl to the new splitted header files

--- final_project/sparse_mlpl/main.c
+++ final_project/sparse_mlpl/main.c
@@ -1,21 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <ctype.h>
-#include "sparse_matrix.h"
-
-void print_elem_vector(elem *vector, int n) {
-	int i;
-	for(i=0; i < n;i++) {
-		printf("%f ", vector[i]);
-	}
-}
-
-void print_int_vector(int *vector, int n) {
-	int i;
-	for(i=0; i < n;i++) {
-		printf("%d ", vector[i]);
-	}
-}
+#include "default_includes.h"
 
 void print_sparse_matrix_data(sparse_matrix_arr *matrix) {
 	int n = matrix->rowptr[matrix->n];
@@ -32,16 +15,10 @@ int main(void) {
 	if ((matrix = allocate_and_read_matrix(stdin)) == NULL) {
 		exit(EXIT_FAILURE);
 	}
-#ifdef DEBUG
-	print_sparse_matrix_data(matrix);
-#endif
-	if ((vector = allocate_and_read_vector(matrix->n)) == NULL) {
+	if ((vector = allocate_and_read_vector(stdin, matrix->n)) == NULL) {
 		free(matrix);
 		exit(EXIT_FAILURE);
 	}
-#ifdef DEBUG
-	print_elem_vector(vector, matrix->n);
-#endif
 	if ((result = allocate_vector(matrix->n)) == NULL) {
 		free_sparse_matrix_arr(matrix);
 		free(vector);
--- final_project/sparse_mlpl/sparse_matrix.c
+++ final_project/sparse_mlpl/sparse_matrix.c
@@ -17,7 +17,7 @@ sparse_matrix_arr* allocate_sparse_matrix_arr(int n, int numNnz) {
 		free(matrix);
 		return(NULL);
 	}
-	matrix->values = (elem *)malloc(numNnz*sizeof(elem));
+	matrix->values = (elem *)calloc(numNnz,sizeof(elem));
 	if (matrix->colind == NULL) {
 		MEMORY_ALLOCATION_FAILURE_AT("allocate_sparse_matrix_arr: matrix->values");
 		free(matrix->colind);
@@ -64,76 +64,4 @@ elem *allocate_vector(int n) {
 		vector[i] = 0;
 	}
 	return vector;
-}
-
-elem *allocate_and_read_vector(int n) {
-	int i;
-	double scanf_receptor;
-	elem *vector;
-	if ((vector = allocate_vector(n)) == NULL) {
-		return NULL;
-	}
-	for(i=0; i<n; i++) {
-		if (scanf("%lf", &scanf_receptor) < 1) {
-			fprintf(stderr, "Error reading vector value in pos: %d", i);
-			free(vector);
-			return NULL;
-		}
-		vector[i] = scanf_receptor;
-	}
-	return vector;
-}
-
-sparse_matrix_arr *allocate_and_read_matrix(FILE *fp) {
-	int n=0, numNnz=0;
-	int i, j, val_index, pos;
-	double scanf_receptor;
-	elem *values;
-	sparse_matrix_arr* matrix;
-	if (fscanf(fp, "%d", &n) < 1) {
-		fprintf(stderr, "Error reading matrix size");
-		return NULL;
-	}
-#ifdef DEBUG
-	printf("matrix size: %d\n", n);
-#endif
-	values = malloc(n*n*sizeof(elem));
-	if (values == NULL) {
-		MEMORY_ALLOCATION_FAILURE_AT("read_matrix: values");
-		return NULL;
-	}
-	for(j=0; j<n; j++) {
-		for(i=0; i<n; i++) {
-			pos = i + (j*n);
-			if (fscanf(fp, "%lf", &scanf_receptor) < 1) {
-				fprintf(stderr, "Error reading matrix values at pos: %d\n", pos);
-				free(values);
-				return NULL;
-			}
-			values[pos] = scanf_receptor;
-			if (values[pos] != 0)
-				numNnz++;
-		}
-	}
-		
-	if ((matrix = allocate_sparse_matrix_arr(n, numNnz)) == NULL)
-		return NULL;
-	val_index = 0;
-	matrix->rowptr[0] = 0;
-	for(j=0; j<n; j++) {
-		for(i=0; i<n; i++) {
-			pos = i + (j*n);
-			if (values[pos] != 0) {
-				matrix->values[val_index] = values[pos];
-				matrix->colind[val_index] = i;
-#ifdef DEBUG
-				printf("Found new non-0 val: %lf, colind: %d, val_index:%d\n", values[pos], matrix->colind[val_index], val_index);
-#endif
-				val_index++;
-			}
-		}
-		matrix->rowptr[j+1] = val_index;
-	}
-	free(values);
-	return matrix;
 }
