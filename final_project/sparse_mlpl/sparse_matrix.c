b5cad1e93a843a48b6fabb99c6b28ffceed70163
neer <neer@neer-laptop.(none)>
2009-08-21 23:00:45 +0300

    improved mult_sparse_arr

--- final_project/sparse_mlpl/sparse_matrix.c
+++ final_project/sparse_mlpl/sparse_matrix.c
@@ -32,6 +32,7 @@ sparse_matrix_arr* allocate_sparse_matrix_arr(int n, int numNnz) {
 		free(matrix);
 		return NULL;
 	}
+	matrix->rowptr[matrix->n] = numNnz;
 	return matrix;
 }
 
@@ -44,12 +45,10 @@ void free_sparse_matrix_arr(sparse_matrix_arr* matrix) {
 
 void  mult_sparse_arr(const sparse_matrix_arr *A, const elem* v, elem* result) {
 	int j, val_index;
-	val_index = 0;
-	for(j=0; j < A->n; j++) {
-		result[j] = 0;
-		for(; val_index<A->rowptr[j+1]; val_index++) {
-			result[j] = result[j] + (A->values[val_index]*v[A->colind[val_index]]);
-		}
+	for(val_index=0, j=0; val_index<A->rowptr[A->n]; val_index++) {
+		result[j] = result[j] + (A->values[val_index]*v[A->colind[val_index]]);
+		while (val_index+1>=A->rowptr[j+1])
+			j++;
 	}
 }
 
